9dc6303dc45f260fda067752aa4d21301deef71c
Meryem R <82731366+Meryem-R@users.noreply.github.com>
2023-04-28 17:47:07 -0400

    Add buttons and display scores

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,56 @@
 
 
-function getComputerChoice(){
-    
+function getComputerChoice() {
+
     let choice = ['paper', 'scissors', 'rock'];
     let randomChoice = Math.floor(Math.random() * choice.length);
     return choice[randomChoice];
 }
 
-function getPlayerChoice(){
+function OneRound(computerChoice, playerChoice) {
 
-    return prompt("What's your pick?").toLowerCase();
-}
 
-function playRound(computerChoice, playerChoice){
 
-    if(playerChoice == computerChoice)
-            return "It's a tie!"
-    else
-        if(playerChoice == 'rock' && computerChoice == 'scissors')
-            return `You win! Rock beats Scissor`;
-        if(playerChoice == 'paper' && computerChoice == 'rock')
-            return `You win! Paper beats Rock`;
-        if(playerChoice == 'scissor' && computerChoice == 'paper')
-            return `You win! scissor beats paper`;
-    return `You lose! ${computerChoice} beats ${playerChoice}!`;
-                
-}
-
-function game(){
-    let rounds = 5;
-
-    for(let i = 1; i <= rounds; i++){ 
-        
-        console.log(`Round ${i}`);
-
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        
-        console.log(`The computer chooses ${computerChoice}!`);
-        console.log(playRound(computerChoice, playerChoice));
+    if (playerChoice == computerChoice) {
+        result.textContent = `Computer chooses ${computerChoice}, It's a tie!`;
     }
+    else
+        if (playerChoice == 'rock' && computerChoice == 'scissors') {
+            result.textContent = `Computer chooses scissors, You win!`;
+            playerScore++;
+        }
+        else
+            if (playerChoice == 'paper' && computerChoice == 'rock') {
+                result.textContent = `Computer chooses rock, You win!`;
+                playerScore++;
+            }
+            else
+                if (playerChoice == 'scissors' && computerChoice == 'paper') {
+                    result.textContent = `Computer chooses paper, You win!`;
+                    playerScore++;
+                }
+                else {
+                    result.textContent = `Computer chooses ${computerChoice}, You lose!`;
+                    computerScore++;
+                }
+
 }
 
-game();
+
+let computerScore = 0;
+let playerScore = 0;
+
+let result = document.querySelector(".result");
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener("click", () => {
+    OneRound(getComputerChoice(), button.classList[0]);
+    document.querySelector('.score').textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+
+    if (computerScore == 5)
+        result.textContent = "Computer wins!";
+    if (playerScore == 5) {
+        result.textContent = "Player wins!";
+    }
+}));
+
